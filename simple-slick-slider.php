dd2a27db31948e6a04e73346496011fc6ce8b8ae
Muzammil Hussain <muzammil.hussain@gharpar.co>
2020-05-24 01:39:18 +0500

    Enabling babaljs and converting gulfile to ES6 syntax

--- simple-slick-slider.php
+++ simple-slick-slider.php
@@ -20,6 +20,15 @@ define('SSS_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('SSS_PLUGIN_MAIN_PATH', plugin_dir_path(__FILE__));
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Admin Enqueue
+////////////////////////////////////////////////////////////////////////////////////////////////////
+function sss_admin_enqueue() {
+	wp_enqueue_script('sss-bootstrap-admin-loader', SSS_PLUGIN_URL . '', ['jquery'], SSS_VERSION, ture );
+}
+add_action('wp_admin_enqueue', 'sss_admin_enqueue');
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 /// Post types
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -103,12 +112,65 @@ function simple_slick_slider_metabox_callback ( $post ) {
 	$value = get_post_meta( $post->ID, 'my_key', true ); //my_key is a meta_key. Change it to whatever you want
 
 	?>
-	<div class="panel-wrap product_data">
-		<p>
-			<label for="wdm_new_field"><?php _e( "Choose value:", 'choose_value' ); ?></label><br />
-			<input type="radio" name="the_name_of_the_radio_buttons" value="custom" <?php checked( $value, 'custom' ); ?> >Custom
-			<input type="radio" name="the_name_of_the_radio_buttons" value="wpposts" <?php checked( $value, 'wpposts' ); ?> >Posts
-		</p>
+	<div class="panel-wrap slides-source">
+		<?php // @TODO slider source selection ?>
+<!--		<div class="slides-source">-->
+<!--			<p>-->
+<!--				<label for="wdm_new_field">--><?php //_e( "Choose value:", 'choose_value' ); ?><!--</label><br />-->
+<!--				<input type="radio" name="the_name_of_the_radio_buttons" value="custom" --><?php //checked( $value, 'custom' ); ?><!-- >Custom-->
+<!--				<input type="radio" name="the_name_of_the_radio_buttons" value="wpposts" --><?php //checked( $value, 'wpposts' ); ?><!-- >Posts-->
+<!--			</p>-->
+<!--		</div>-->
+		<div class="slide-pannels">
+			<p>
+				<button class="button button-primary">Add a slide</button>
+			</p>
+			<div class="accordion" id="accordionExample">
+				<div class="card">
+					<div class="card-header" id="headingOne">
+						<h2 class="mb-0">
+							<button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+								Collapsible Group Item #1
+							</button>
+						</h2>
+					</div>
+
+					<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+						<div class="card-body">
+							Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+						</div>
+					</div>
+				</div>
+				<div class="card">
+					<div class="card-header" id="headingTwo">
+						<h2 class="mb-0">
+							<button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+								Collapsible Group Item #2
+							</button>
+						</h2>
+					</div>
+					<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+						<div class="card-body">
+							Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+						</div>
+					</div>
+				</div>
+				<div class="card">
+					<div class="card-header" id="headingThree">
+						<h2 class="mb-0">
+							<button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+								Collapsible Group Item #3
+							</button>
+						</h2>
+					</div>
+					<div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+						<div class="card-body">
+							Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 	<?php
 }
